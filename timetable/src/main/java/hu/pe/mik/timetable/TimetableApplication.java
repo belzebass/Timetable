51a8a3cbfd95cc2a3f94178d84879a2f3463a477
A_Norbert <anorbert99@gmail.com>
2020-02-20 15:23:46 +0100

    refs #1

---
+++ timetable/src/main/java/hu/pe/mik/timetable/TimetableApplication.java
@@ -0,0 +1,13 @@
+package hu.pe.mik.timetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimetableApplication.class, args);
+	}
+
+}
---
+++ timetable/src/test/java/hu/pe/mik/timetable/TimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.pe.mik.timetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
